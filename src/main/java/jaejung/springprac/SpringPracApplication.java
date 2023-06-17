0f1a998db2e6c2801116c84b54bab60b9f5ffc6d
Jaejung <ljj0512@gmail.com>
2023-06-17 15:59:40 +0900

    add swagger-ui

--- src/main/java/jaejung/springprac/SpringPracApplication.java
+++ src/main/java/jaejung/springprac/SpringPracApplication.java
@@ -2,6 +2,7 @@ package jaejung.springprac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringPracApplication {
@@ -12,4 +13,14 @@ public class SpringPracApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPracApplication.class, args);
 	}
+
+//	@Bean
+//	public Docket api() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.select()
+//				.apis(RequestHandlerSelectors.basePackage("jaejung.springprac"))
+//				.apis(RequestHandlerSelectors.any())
+//				.paths(PathSelectors.any())
+//				.build();
+//	}
 }
